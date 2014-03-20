cf392db1231d670b2273d63a765c879fbe784056
stevenlauwers22 <stevenlauwers22@hotmail.com>
2014-03-20 14:25:36 +0100

    Create CI build task

--- ALM.Demo.Tests/HomeController_Index_Tests.cs
+++ ALM.Demo.Tests/HomeController_Index_Tests.cs
@@ -1,10 +1,11 @@
 ﻿using System.Web.Mvc;
 using ALM.Demo.Controllers;
 using ALM.Demo.Models.Home;
-using Xunit;
+using NUnit.Framework;
 
 namespace ALM.Demo.Tests
 {
+    [TestFixture]
     public class HomeController_Index_Tests
     {
         private readonly ActionResult _result;
@@ -14,25 +15,25 @@ namespace ALM.Demo.Tests
             _result = new HomeController().Index();
         }
 
-        [Fact]
+        [Test]
         public void Returns_a_ViewResult()
         {
             Assert.IsAssignableFrom<ViewResult>(_result);
         }
 
-        [Fact]
+        [Test]
         public void Passes_an_IndexModel_to_the_View()
         {
             var viewResult = (ViewResult)_result;
             Assert.IsAssignableFrom<IndexModel>(viewResult.Model);
         }
 
-        [Fact]
+        [Test]
         public void Puts_a_Message_and_Version_on_the_Model()
         {
             var viewResult = (ViewResult)_result;
             var model = (IndexModel)viewResult.Model;
-            Assert.Equal(string.Format("Hello world from Tests"), model.Message);
+            Assert.AreEqual(string.Format("Hello world from Tests"), model.Message);
             Assert.NotNull(model.Version);
         }
     }
